99b0ba2c49f2e3b5677ee93a0a177db7098c30ec
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2023-12-29 16:46:05 +0530

    Reads Auth Cookie in the Loader of Canvases Route

--- app/routes/canvases._index/route.tsx
+++ app/routes/canvases._index/route.tsx
@@ -1,3 +1,13 @@
+import { authCookie } from "~/auth";
+import { LoaderFunctionArgs } from "@remix-run/node";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const cookieString = request.headers.get("Cookie");
+  const userId = await authCookie.parse(cookieString);
+
+  return { userId };
+};
+
 const Canvases = () => {
   return <div>Canvases Screen</div>;
 };
