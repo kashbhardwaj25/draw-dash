c470a8a0c21d8d7b30423de452fba25d701a5acc
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2023-12-31 23:32:50 +0530

    Refactor canvas creation form

--- app/routes/canvases._index/route.tsx
+++ app/routes/canvases._index/route.tsx
@@ -14,18 +14,28 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
 const Canvases = () => {
   return (
-    <div className="flex justify-between items-center m-4">
-      <div className="text-2xl font-bold mb-4">Canvases</div>
-      <div className="flex gap-4">
-        <form method="post" action="/create-canvas">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Create Canvas
-          </button>
-        </form>
+    <div>
+      <div className="flex justify-between items-center m-4">
+        <div className="text-2xl font-bold mb-4">Canvases</div>
         <form method="post" action="/logout">
           <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
             Logout
           </button>
+        </form>
+      </div>
+      <div className="m-4">
+        <form method="post" action="/create-canvas">
+          <div className="flex gap-4">
+            <input
+              type="text"
+              name="name"
+              placeholder="Canvas Name"
+              className="border border-gray-400 rounded-lg p-2"
+            />
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Create Canvas
+            </button>
+          </div>
         </form>
       </div>
     </div>
