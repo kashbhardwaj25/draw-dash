7ed153f41e4a3b55dec39f29b0fe7fcf4145a63b
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2023-12-31 20:59:31 +0530

    Refactor index page layout and add signup and login links

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,9 +11,21 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <Link to="/signup">Signup</Link>
-      <Link to="/login">Login</Link>
+    <div
+      style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}
+      className="font-sans text-lg flex flex-col items-center justify-center h-screen space-y-4"
+    >
+      <h1 className="text-4xl font-bold text-blue-700">DRAWDASH</h1>
+      <p className="text-center text-gray-600">
+        Forge ideas, diagrams, notes, and art in your limitless cloud canvas.
+        Collaborate, embed, export - unleash your digital renaissance.
+      </p>
+      <Link to="/signup" className="text-blue-500 hover:text-blue-700">
+        Signup
+      </Link>
+      <Link to="/login" className="text-blue-500 hover:text-blue-700">
+        Login
+      </Link>
     </div>
   );
 }
