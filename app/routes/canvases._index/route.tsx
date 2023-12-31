77ba11c322fd11a3bda0c308306cab5b545d95f7
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2023-12-31 22:25:05 +0530

    Update Canvases screen layout and add logout button

--- app/routes/canvases._index/route.tsx
+++ app/routes/canvases._index/route.tsx
@@ -14,10 +14,12 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
 const Canvases = () => {
   return (
-    <div>
-      <div>Canvases Screen</div>
+    <div className="flex justify-between items-center m-4">
+      <div className="text-2xl font-bold mb-4">Canvases</div>
       <form method="post" action="/logout">
-        <button>Logout</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Logout
+        </button>
       </form>
     </div>
   );
