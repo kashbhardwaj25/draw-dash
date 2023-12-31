357f1e724f62d5756caaa376a53d54e5c8e46092
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2023-12-31 23:42:48 +0530

    Add canvas creation functionality

--- app/routes/create-canvas.ts
+++ app/routes/create-canvas.ts
@@ -1,11 +1,11 @@
 import { ActionFunctionArgs, redirect } from "@remix-run/node";
+import { createCanvas } from "./canvases._index/queries";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
-
   const canvasName = String(formData.get("canvasName"));
 
-  console.log(canvasName);
+  await createCanvas(canvasName);
 
   return redirect("/canvases");
 };
